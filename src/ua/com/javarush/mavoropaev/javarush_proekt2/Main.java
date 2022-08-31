48c00d77e65decdcfeebdcdae63d3ffc8a8ee779
mavoropaev <m.a.voropaev@gmail.com>
2022-08-31 12:56:47 +0300

    add jar

--- src/ua/com/javarush/mavoropaev/javarush_proekt2/Main.java
+++ src/ua/com/javarush/mavoropaev/javarush_proekt2/Main.java
@@ -6,7 +6,6 @@ import ua.com.javarush.mavoropaev.javarush_proekt2.dialog.Dialog;
 public class Main {
 
     public static void main(String[] args){
-
         Dialog dialog = Dialog.getInstance();
         dialog.startDialog();
 
